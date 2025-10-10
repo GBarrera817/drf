ead591d6a1e3feae39110c124ada313887789d2f
Gabriela Barrera <g.barrera.angel@gmail.com>
2025-10-10 10:13:33 -0300

    Fix endpoint typo and add status code printout

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,10 @@ endponit = 'https://httpbing.org/anything'
 
 
 get_response = requests.get(endpoint)  # HTTP Request
+# get_response = requests.get(endpoint, json={'query:"Hello world"'})  # HTTP Request
 print(get_response.text)  # Print raw text response
+print(get_response.json())
+print(get_response.status_code)
 
 
 """
