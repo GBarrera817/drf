cc636a79f9dbfa351b5c694172de1235de893910
Gabriela Barrera <g.barrera.angel@gmail.com>
2025-10-13 15:21:31 -0300

    Use of Product property and methods, serializers and custom methods in the serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(['GET'])
 def api_home(request, *args, **kwargs):
@@ -12,15 +13,12 @@ def api_home(request, *args, **kwargs):
     DRF API View
     """
 
-    if request.method != 'POST':
-        return Response({'detail': 'GET not allowed'}, status=405)
-
-    model_data = Product.objects.all().order_by('?').first()
+    instance = Product.objects.all().order_by('?').first()
     data = {}
 
-    if model_data:
+    if instance:
 
-        data = model_to_dict(model_data, fields=['id', 'title'])
+        data = ProductSerializer(instance).data
 
     return Response(data)
 
