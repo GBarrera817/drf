0ec1082e787702415f6e49766efab68e3e5e7d70
Gabriela Barrera <g.barrera.angel@gmail.com>
2025-10-20 15:52:18 -0300

    Authentication_classes and Permission_classes

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     # pk is the kwarg
-    # path('', views.product_list_create_view),
-    path('', views.product_mixin_view),
+    path('', views.product_list_create_view),
+    # path('', views.product_mixin_view),
     path('<int:pk>/update/', views.product_update_view),
     path('<int:pk>/delete/', views.product_destroy_view),
-    # path('<int:pk>/', views.product_detail_view),  # is the same with path('<int:pk>/', views.ProductDetailAPIView.as_view()),
-    path('<int:pk>/', views.product_mixin_view),  # is the same with path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('<int:pk>/', views.product_detail_view),  # is the same with path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    # path('<int:pk>/', views.product_mixin_view),  # is the same with path('<int:pk>/', views.ProductDetailAPIView.as_view()),
 
     # only for test the view
     # path('', views.product_alt_view),
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins
+from rest_framework import authentication, generics, mixins, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -14,6 +14,8 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
